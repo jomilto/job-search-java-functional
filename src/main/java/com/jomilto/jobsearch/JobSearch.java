310954106d9518aae217e213643572e552b73f24
Jose Lobato <joselobatof@gmail.com>
2021-08-07 18:56:39 -0500

    CLI Commander and CLI Arguments

--- src/main/java/com/jomilto/jobsearch/JobSearch.java
+++ src/main/java/com/jomilto/jobsearch/JobSearch.java
@@ -1,7 +1,13 @@
 package com.jomilto.jobsearch;
 
+import com.beust.jcommander.JCommander;
+import com.jomilto.jobsearch.cli.CLIArguments;
+
+import static com.jomilto.jobsearch.CommanderFunctions.buildCommanderWithName;
+
 public class JobSearch {
     public static void main(String[] args) {
-        System.out.println("Hola");
+        JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
+
     }
 }
