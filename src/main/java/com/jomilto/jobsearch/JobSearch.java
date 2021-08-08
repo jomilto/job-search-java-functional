8f5472247bce10b97996805165e7bcd0091b9f4b
Jose Lobato <joselobatof@gmail.com>
2021-08-07 19:46:34 -0500

    CLI Mapping

--- src/main/java/com/jomilto/jobsearch/JobSearch.java
+++ src/main/java/com/jomilto/jobsearch/JobSearch.java
@@ -1,13 +1,46 @@
 package com.jomilto.jobsearch;
 
 import com.beust.jcommander.JCommander;
+import com.jomilto.jobsearch.api.APIJobs;
 import com.jomilto.jobsearch.cli.CLIArguments;
+import com.jomilto.jobsearch.cli.CLIFunctions;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 import static com.jomilto.jobsearch.CommanderFunctions.buildCommanderWithName;
+import static com.jomilto.jobsearch.CommanderFunctions.parseArguments;
+import static com.jomilto.jobsearch.api.APIFunctions.buildAPI;
 
 public class JobSearch {
     public static void main(String[] args) {
         JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
 
+        Stream<CLIArguments> streamOfCLI =
+                parseArguments(jCommander, args, JCommander::usage)
+                        .orElse(Collections.emptyList())
+                        .stream()
+                        .map(obj -> (CLIArguments) obj);
+
+        Optional<CLIArguments> cliArgumentsOptional =
+                streamOfCLI.filter(cli -> !cli.isHelp())
+                        .filter(cli->cli.getKeyword() != null)
+                        .findFirst();
+
+        cliArgumentsOptional.map(CLIFunctions::toMap)
+                .map(JobSearch::executeRequest)
+                .orElse(Stream.empty())
+                .forEach(System.out::println);
+    }
+
+    private static Stream<JobPosition> executeRequest(Map<String,Object> params){
+        APIJobs api = buildAPI(APIJobs.class, "https://jobs.github.com");
+
+        return Stream.of(params)
+                .map(api::jobs)
+                .flatMap(Collection::stream);
     }
 }
